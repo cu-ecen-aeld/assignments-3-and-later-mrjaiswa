0e734167155f1a2fbc070fadb5847d3f3ce3fd9f
Mrinal Jaiswal <mrinal.jaiswal@colorado.edu>
2022-09-28 21:15:40 -0500

    Added change

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -44,7 +44,7 @@ fi
 #echo "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
-
+# Added the writer 
 for i in $( seq 1 $NUMFILES)
 do
 	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
