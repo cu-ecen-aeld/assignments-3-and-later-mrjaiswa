3e3166085b2948a52946f9007409e6f024b81999
Mrinal Jaiswal <mrinal.jaiswal@colorado.edu>
2024-02-28 12:02:37 -0600

    Changing start stop script to manipulate qemu

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,15 +1,34 @@
-case "$1" in 
-    start) 
-        echo "Starting aesdsocket" 
-        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d 
-        ;; 
-    stop) 
-        echo "Stopping aesdsocket" 
-        start-stop-daemon -K -n aesdsocket 
-        ;; 
-    *) 
-        echo "Usage: $0 {start|stop}" 
-    exit 1 
-esac 
+#!/bin/sh
 
-exit 0 
+FLAG_FILE="/var/run/aesdsocket.started"
+
+case "$1" in
+    start)
+        if [ ! -e "$FLAG_FILE" ]; then
+            echo "Aesdsocket called for the first time"
+            
+            touch "$FLAG_FILE"
+        else
+            
+            echo "Starting aesdsocket for the first time"
+            start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+            touch "$FLAG_FILE"
+        fi
+        ;;
+    stop)
+        if ps aux | grep -q '[a]esdsocket'; then
+            echo "Stopping aesdsocket"
+            start-stop-daemon -K -n aesdsocket
+            rm -f "$FLAG_FILE"
+        else
+            echo "No aesdsocket is running"
+
+        fi
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
